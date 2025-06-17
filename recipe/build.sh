e3cacb44655cd1f2c804ca7f00d24dade841329a
Daniel Nachun <daniel.nachun@gmail.com>
2025-06-17 09:40:33 -0700

    remove symlinks

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
+    --no-bin-links \
     --global \
     --build-from-source \
     ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
@@ -13,10 +14,24 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
+mkdir -p ${PREFIX}/bin
+
+tee ${PREFIX}/bin/capacitor << EOF
+#!/bin/sh
+exec ${PREFIX}/lib/node_modules/@capacitor/cli/bin/capacitor
+EOF
+chmod +x ${PREFIX}/bin/capacitor
+
+tee ${PREFIX}/bin/cap << EOF
+#!/bin/sh
+exec ${PREFIX}/lib/node_modules/@capacitor/cli/bin/capacitor
+EOF
+chmod +x ${PREFIX}/bin/cap
+
 tee ${PREFIX}/bin/capacitor.cmd << EOF
-call %CONDA_PREFIX%\bin\node %PREFIX%\bin\capacitor %*
+call %CONDA_PREFIX%\bin\node %PREFIX%\lib\node_modules\@capacitor\cli\bin\capacitor %*
 EOF
 
 tee ${PREFIX}/bin/cap.cmd << EOF
-call %CONDA_PREFIX%\bin\node %PREFIX%\bin\cap %*
+call %CONDA_PREFIX%\bin\node %PREFIX%\lib\node_modules\@capacitor\cli\bin\capacitor %*
 EOF
